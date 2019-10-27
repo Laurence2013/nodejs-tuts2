d79606c54d55e592e2cee0dfe4141e798aea6702
Laurence2013 <djroa1985@gmail.com>
2019-10-27 20:44:11 +0000

    10-commit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
-const express = require('express');
+const express     = require('express');
+const bodyParser  = require('body-parser');
 
 const app = express();
 
-app.use('/users',(req,res,next) => {
-  console.log('Hello 2');
-  res.send('<h2>Hello world 2</h2>');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product',(req,res,next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/',(req,res,next) => {
-  console.log('Hello 1');
-  res.send('<h2>Hello world 1</h2>');
+  res.send('<h1>Hello from Express</h1>');
 });
 
 app.listen(3000);
