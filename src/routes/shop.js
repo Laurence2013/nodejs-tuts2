be00321883dd66557e0165a5a549fe00bb740b4e
Laurence2013 <djroa1985@gmail.com>
2019-10-27 21:18:22 +0000

    12-commit

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -1,1 +1,9 @@
-console.log('shop');
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/',(req,res,next) => {
+  res.send('<h1>Hello from Express</h1>');
+});
+
+module.exports = router;
