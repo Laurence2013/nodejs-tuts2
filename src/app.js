b16624fdadec8488a70509c1c683d045cc2af595
Laurence2013 <djroa1985@gmail.com>
2019-10-27 14:55:47 +0000

    7-commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
-const http  = require('http');
+const express = require('express');
 
-const server = http.createServer();
-server.listen(3000);
+const app = express();
+
+app.use('/',(req,res,next) => {
+  console.log('In the middleware 3');
+  next();
+});
+
+app.use('/add-product',(req,res,next) => {
+  console.log('In the middleware 1');
+  res.send('<h1>Hello from Add Product page</h1>');
+});
+
+app.use('/',(req,res,next) => {
+  console.log('In the middleware 2');
+  res.send('<h1>Hello from Express</h1>');
+});
+
+app.listen(3000);
 
