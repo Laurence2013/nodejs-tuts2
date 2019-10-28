fcf193d9b2391eee56f116c6e1ae864fee11fe4f
Laurence2013 <djroa1985@gmail.com>
2019-10-28 22:53:21 +0000

    18-commit

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const path    = require('path');
-const rootdir = require('../utils/path');
+const express   = require('express');
+const path      = require('path');
+const rootdir   = require('../utils/path');
+const adminData = require('./admin');
 
-const router = express.Router();
+const router    = express.Router();
 
 router.get('/',(req,res,next) => {
+  console.log(adminData.products);
   res.sendFile(path.join(rootdir, 'views', 'shop.html'));
 });
 
