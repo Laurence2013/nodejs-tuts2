e625f1b62af3f73a165d46e6a602056bae9870d3
Laurence2013 <djroa1985@gmail.com>
2019-10-29 10:01:38 +0000

    19-commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,13 @@ const adminData   = require('./routes/admin');
 const shopRoutes  = require('./routes/shop');
 const pNotFound   = require('./routes/404');
 const path        = require('path');
+const xhndlbars   = require('express-handlebars');
 
 const app = express();
+
+app.engine('handlebars', xhndlbars());
+app.set('view engine', 'pug');
+app.set('views', 'src/views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
--- src/routes/404.js
+++ src/routes/404.js
@@ -5,7 +5,7 @@ const rootdir = require('../utils/path');
 const router = express.Router();
 
 router.get('*',(req,res,next) => {
-  res.sendFile(path.join(rootdir, 'views', '404.html'));
+  res.render('404', {pageTitle: '404 - Page Not Found'});
 });
 
 module.exports = router;
