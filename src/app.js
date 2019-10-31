4a9b87484fd3717af0cbb4b722c77aec32bd751a
Laurence2013 <djroa1985@gmail.com>
2019-10-31 22:44:01 +0000

    21-commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const path        = require('path');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 
 app.use(bodyParser.urlencoded({extended: false}));
